fc243e6062a92c6fcd0c840af4cf9fb72ee73b88
Josh Humphrey <joshhumphrey@Joshs-Laptop.local>
2024-12-30 08:51:58 -0600

    access email

--- routes/accountRouter.js
+++ routes/accountRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-
+import nodemailer from "nodemailer";
 import db, { sql } from "../db.js";
 export const accountRouter = Router();
 
@@ -81,4 +81,70 @@ accountRouter.put("/update/:id", async (req, res) => {
     res.status(500).json({ success: false, message: "Error updating account" });
     return;
   }
+});
+
+accountRouter.post("/request-access", async (req, res) => {
+  const { email } = req.body;
+
+  if (!email) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Email is required" });
+  }
+  const existingProfile = await db.query(
+    sql`SELECT * FROM profiles WHERE email = ${email}`
+  );
+  if (existingProfile.length > 0) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Email already in use" });
+  }
+
+  try {
+    const result = await db.query(
+      sql`INSERT INTO accessRequests (email) VALUES (${email})`
+    );
+    const insertedRequest = await db.query(
+      sql`SELECT * FROM accessRequests WHERE email = ${email} LIMIT 1`
+    );
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT, // Default port for Mailhog
+      secure: true,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+
+    const mailOptions = {
+      from: "system@ezpdf.app",
+      to: email,
+      subject: `Create your Ezpdf acco`,
+      text: "Your invoice is attached, you better pay it.",
+      html: `<p>Click <a href="http://localhost:3030/account/new/${insertedRequest[0].id}">here</a> to create your account</p>`,
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return console.error("Error sending email:", error);
+      }
+      console.log("Email sent:", info.messageId);
+    });
+    res.status(201).json({ success: true, data: insertedRequest[0] });
+  } catch (error) {
+    console.error("error", error);
+    res
+      .status(500)
+      .json({ success: false, message: "Error requesting access" });
+  }
+});
+
+accountRouter.get("/new/:id", async (req, res) => {
+  let id = req.params.id;
+  const results = await db.query(sql`
+          SELECT * from accessRequests WHERE id = ${id};
+        `);
+  let request = results[0];
+  res.status(200).send(request);
+  return;
 });
